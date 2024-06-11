c0f2c0d2ffd5558cebeed5c094c2ab47fc7d2a60
tian.dai <1033448220@qq.com>
2024-06-11 23:35:37 +0800

    save

--- main.go
+++ main.go
@@ -1,58 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-	"sync"
-)
-
-type CalcuRet struct {
-	Name       string
-	FormatTime string
-}
-
-type TransitStationsResponse struct {
-	Status  int    `json:"status"`
-	Message string `json:"message"`
-	Result  struct {
-		Stations []string `json:"stations"`
-	} `json:"result"`
-}
-
 func main() {
-	res := NewSliceSafer()
-	allStations := getAllStations("shenzhen")
-	//fmt.Printf("%v", lo.Map(allStations, func(item *StationsDetail, index int) string {
-	//	return item.Name + "\n"
-	//}))
-	startLat, startLng, startUID := getStartLocation("高新园")
-	wg := sync.WaitGroup{}
-	limit := make(chan bool, 5)
-	for _, station := range allStations {
-		wg.Add(1)
-		limit <- true
-		go func(startUID string, station *StationsDetail) {
-			defer func() {
-				wg.Done()
-				<-limit
-			}()
-			destLat, destLng, destUID := station.Location.Lat, station.Location.Lng, station.UID
-			det, dur := searchDestStation(startLat, startLng, destLat, destLng, 3600, startUID, destUID)
-			if det == "" {
-				return
-			}
-			res.Append(DetRet{
-				det,
-				dur,
-			})
-		}(startUID, station)
-
-	}
-	wg.Wait()
-	sort.Slice(res.slice, func(i, j int) bool {
-		return res.slice[i].Duration > res.slice[j].Duration
-	})
-	for _, ret := range res.slice {
-		fmt.Printf("到%v %v\n", ret.Name, FormatTime(ret.Duration))
-	}
+	calculateDest("shenzhen", "高新园", 3600)
 }
--- router.go
+++ router.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -38,6 +39,44 @@ type RouteDetail struct {
 		Lng float64 `json:"lng"`
 	} `json:"origin"`
 	Routes []*Route `json:"routes"`
+}
+
+func calculateDest(region, startStation string, duration int64) {
+	res := NewSliceSafer()
+	allStations := getAllStations(region)
+	//fmt.Printf("%v", lo.Map(allStations, func(item *StationsDetail, index int) string {
+	//	return item.Name + "\n"
+	//}))
+	startLat, startLng, startUID := getStartLocation(startStation)
+	wg := sync.WaitGroup{}
+	limit := make(chan bool, 5)
+	for _, station := range allStations {
+		wg.Add(1)
+		limit <- true
+		go func(startUID string, station *StationsDetail) {
+			defer func() {
+				wg.Done()
+				<-limit
+			}()
+			destLat, destLng, destUID := station.Location.Lat, station.Location.Lng, station.UID
+			det, dur := searchDestStation(startLat, startLng, destLat, destLng, duration, startUID, destUID)
+			if det == "" {
+				return
+			}
+			res.Append(DetRet{
+				det,
+				dur,
+			})
+		}(startUID, station)
+
+	}
+	wg.Wait()
+	sort.Slice(res.slice, func(i, j int) bool {
+		return res.slice[i].Duration > res.slice[j].Duration
+	})
+	for _, ret := range res.slice {
+		fmt.Printf("到%v %v\n", ret.Name, FormatTime(ret.Duration))
+	}
 }
 
 func getStartLocation(name string) (float64, float64, string) {
